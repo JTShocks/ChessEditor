383b32c9d1bbd797c8ef8469f7687296fa8f483c
Dylan Torres <glaciershield@gmail.com>
2024-08-30 19:13:33 -0400

    Pathway Fixes
    Paths now appear using OnDrawGizmosSelected.

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -30,7 +30,6 @@ public class Piece : MonoBehaviour
 
             Gizmos.color = pathColor;
             Gizmos.DrawLineStrip(localPaths, false);
-            //Gizmos.DrawSphere( (insert Vector3 data here) , 0.25f);
         }
 
 
